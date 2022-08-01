20e9353243f33e2222673029f4e32832bd539dd0
Gisele Gomes <gselegomes@gmail.com>
2022-08-01 11:49:38 -0300

    add new to-do input

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,10 +1,37 @@
 <template>
-    <h1>To-do List</h1>
-    <form class="form">
-        <div>
-            <label class="form__label" >Today I need to</label>
-            <input class="form__input" type="text" size="30" required>
-            <button class="button">Submit</button>
+    <section>
+        <div class="heading">
+            <h1 class="heading__title">To-do List</h1>
         </div>
-  </form>
+        <form class="form">
+            <label class="form__label">Today I need to:</label>
+            <input v-model="currentTodo" class="form__input" type="text" size="30" required>
+            <button v-on:click.prevent="newTodo" class="form__button">Submit</button>
+        </form>
+        <div class="list">
+            <ul>
+                <li v-for="todo in todos" :key="todo.text">{{ todo.text }}</li>
+            </ul>
+        </div>
+    </section>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentTodo: "",
+            todos: []
+        }
+    },
+    methods: {
+        newTodo() {
+            this.todos.push({
+                text: this.currentTodo,
+                done: false
+            });
+            this.currentTodo = "";
+        }
+    }
+}
+</script>
