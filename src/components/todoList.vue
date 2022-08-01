a10154238e0769aed2a235395c52dd4ad04110d6
Gisele Gomes <gselegomes@gmail.com>
2022-08-01 10:43:26 -0300

    add to-do list structure

---
+++ src/components/todoList.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>To-do List</h1>
+    <form class="form">
+        <div>
+            <label class="form__label" >Today I need to</label>
+            <input class="form__input" type="text" size="30" required>
+            <button class="button">Submit</button>
+        </div>
+  </form>
+</template>
